eab4f61caa422ed5c12c1424806b63118ebcc536
arshathkhan2006 <mhdarshathkhan@gmail.com>
2026-09-21 16:07:45 +0530

    DAY_1

---
+++ Ex.java
@@ -0,0 +1,12 @@
+public class Ex {
+    public static void main(String[] args){
+        String name = "Arshath";
+        int age = 20;
+        float height = 41.5f;
+        double weight = 75.6;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+    }
+    }
---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        String name = "Arshath";
+        int age = 20;
+        double height = 21;
+        float weight = 80.6f;
+        char sec = 'a';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(sec);
+    }
+}
